409c3eb260966d8f0307e36febc2abcfa85adc46
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-04-07 21:45:44 -0500

    working on middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,23 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers/index";
-import { ADD_TODO } from "./actions/todo";
+import { ADD_TODO, REMOVE_TODO } from "./actions/todo";
 
 //middleware
 const logger = (state) => (next) => (action) => {
-  console.log("Ha ocurrido una nueva accion", action);
+  console.log("ha ocurrido una accion" + action);
+  next(action);
+};
 
+const confirmDeleteToDo = (state) => (next) => (action) => {
+  // console.log("Ha ocurrido una nueva accion", action);
+  if (action.type === REMOVE_TODO) {
+    let conf = window.confirm("Seguro deseas eliminar?");
+    if (conf) {
+      next(action);
+    }
+  } else {
+    next(action);
+  }
   //   setTimeout(() => {
   //     next(action);
   //   }, 1000);
@@ -26,7 +38,10 @@ const logger = (state) => (next) => (action) => {
 
 //Store
 // almacenamiento de nuestro estado
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(confirmDeleteToDo, logger)
+);
 
 export default store;
 
